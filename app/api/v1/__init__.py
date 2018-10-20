140353b63252795071be3bff3fde9c0d97720012
jerusha thuo <thuo254jerusha@gmail.com>
2018-10-20 17:12:03 +0300

    User Login and Registration

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 from app.api.v1.views import Sales,SingleSales
 from app.api.v1.views import Products,GetSingleProduct
-#from api.v1.views.sales import Sales, SingleSales
+from app.api.v1.users import UserRegistration,UserLogin
 
 version1  = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(version1)
@@ -12,3 +12,5 @@ api.add_resource(Sales, '/sales')
 api.add_resource(SingleSales, '/sales/<int:sales_id>')
 api.add_resource(Products, '/products')
 api.add_resource(GetSingleProduct, '/products/<int:productId>')
+api.add_resource(UserRegistration, '/registration')
+api.add_resource(UserLogin, '/login')
