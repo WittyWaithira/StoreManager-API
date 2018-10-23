9dc5d0d521b0c3765893a34d2afd673ec83bc49f
jerusha thuo <thuo254jerusha@gmail.com>
2018-10-23 07:54:43 +0300

    Gunicorn and Procfile addition

--- app/tests/v1/tests_views.py
+++ app/tests/v1/tests_views.py
@@ -1,1 +1,22 @@
+import unittest
+import json
+from app import create_app
 
+
+class TestProduct(unittest.TestCase):
+
+    def setUp(self):
+    
+        self.app = create_app()
+        self.app.testing = True
+        self.client = self.app.test_client()
+        self.products = {
+            "productId":"123",
+            "category":"kitchenware",
+            "name":"pan"
+        }
+def test_get_product(self):
+    pass
+
+ def test_getSingleProduct(self):
+     pass
