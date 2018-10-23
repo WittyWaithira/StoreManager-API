61578494e6b5080119917e2dca1010baab3b10fd
jerusha thuo <thuo254jerusha@gmail.com>
2018-10-23 21:17:34 +0300

    Test for products

--- app/tests/v1/tests_views.py
+++ app/tests/v1/tests_views.py
@@ -11,12 +11,25 @@ class TestProduct(unittest.TestCase):
         self.app.testing = True
         self.client = self.app.test_client()
         self.products = {
-            "productId":"123",
             "category":"kitchenware",
             "name":"pan"
         }
     def test_get_product(self):
-        pass
+        with self.client:
+            response = self.client.get(
+                '/api/v1/products',
+                headers=dict(Authorization="Bearer " + token),
+                content_type='application/json'
+            )
+            result = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(response.status_code, 200, result['Products'])
 
     def test_getSingleProduct(self):
-        pass
+        with self.client:
+            response = self.client.get(
+                '/api/v1/products/1',
+                headers=dict(Authorization="Bearer " + token),
+                content_type='application/json'
+            )
+            result = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(response.status_code, 200, result['response'])
